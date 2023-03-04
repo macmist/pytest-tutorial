856b215154a258e9f551b22990e38a6d6ffcdaa4
Quentin MUSY <quentin.musy@gmail.com>
2023-03-04 17:21:05 +0000

    added parametrized tests

--- src/calculator.py
+++ src/calculator.py
@@ -1,3 +1,6 @@
 class Calculator:
     def __init__(self):
         pass
+
+    def add(self, a: int, b: int) -> int:
+        return a + b
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -3,3 +3,25 @@ from src.calculator import *
 
 def test_calculator_creation():
     assert Calculator() is not None
+
+
+add_cases = [
+    (1, 1, 2),
+    (1, 2, 3),
+    (100, 100, 200),
+    (2000, 10, 2010),
+    (222, 111, 333),
+    (200, -200, 0)
+]
+
+
+def test_addition_one_one():
+    assert Calculator().add(1, 1) == 2
+
+
+def test_addition_one_two():
+    assert Calculator().add(1, 2) == 3
+
+@pytest.mark.parametrize('a,b,result', add_cases)
+def test_addition_parametrized(a,b, result):
+    assert Calculator().add(a,b) == result
